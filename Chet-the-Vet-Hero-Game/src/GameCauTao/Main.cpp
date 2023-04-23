c793191c54d234131472c08a7ce7c206c6a9c401
MikoChristmann99 <mchristm21@gmail.com>
2023-04-23 10:53:37 -0500

    Create Main.cpp

---
+++ Chet-the-Vet-Hero-Game/src/GameCauTao/Main.cpp
@@ -0,0 +1,8 @@
+#include "MarioSample/MarioSampleGame.h"
+#include "Castlevania/CastlevaniaGame.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//MarioSample::MarioSampleGame{}.Run();
+	Castlevania::CastlevaniaGame{}.Run();
+}
